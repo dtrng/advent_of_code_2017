a0c64fbec9adfd4518334f890ebe2c9efb64c6dd
David Trång <david.trang@leanon.se>
2017-12-04 22:48:38 +0100

    Added valid tests for second problem of day 3

--- 3/b.test.js
+++ 3/b.test.js
@@ -1,41 +1,21 @@
-const {solver} = require('./a')
+const {solver} = require('./b')
 
 test('example 1', () => {
-  const n = 1
-  expect(solver({n})).toBe(0)
+  const n = 807
+  expect(solver({n})).toBe(880)
 })
 
 test('example 2', () => {
-  const n = 12
-  expect(solver({n})).toBe(3)
+  const n = 881
+  expect(solver({n})).toBe(931)
 })
 
 test('example 3', () => {
-  const n = 23
-  expect(solver({n})).toBe(2)
+  const n = 932
+  expect(solver({n})).toBe(957)
 })
 
 test('example 4', () => {
-  const n = 1024
-  expect(solver({n})).toBe(31)
-})
-
-test('other 1', () => {
-  const n = 21
-  expect(solver({n})).toBe(4)
-})
-
-test('other 2', () => {
-  const n = 19
-  expect(solver({n})).toBe(2)
-})
-
-test('other 3', () => {
-  const n = 13
-  expect(solver({n})).toBe(4)
-})
-
-test('other 4', () => {
-  const n = 961
-  expect(solver({n})).toBe(30)
+  const n = 958
+  expect(solver({n})).toBe(1968)
 })
